85eab3097ae9aa7fd260dcdddd26665933f01903
Sergey-la <bogdanetsserg@yandex.ru>
2022-05-05 21:41:15 +0300

    забыл компонент Categories

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Burgers, Header } from '../../Components';
+import { Burgers, Header, Categories } from '../../Components';
 
 function Main() {
     return (
         <div>
             <Header />
+            <Categories />
             <Burgers />
         </div>
     );
